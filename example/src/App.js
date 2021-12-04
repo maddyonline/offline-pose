a6b46daae95014981385c3a3062abfc88917d88d
Madhav Jha <madhavjha@gmail.com>
2021-12-04 11:02:02 -1000

    using pubsub

--- example/src/App.js
+++ example/src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 import { ExampleComponent } from 'offline-pose'
 import 'offline-pose/dist/index.css'
+import PubSub from 'pubsub-js'
+
 
 class Saver {
   constructor() {
@@ -10,18 +12,38 @@ class Saver {
 }
 
 
+const SubComponent = () => {
+  const [noseY, setNoseY] = React.useState(null);
+  console.log("[component] rendered");
+  React.useEffect(() => {
+    var mySubscriber = function (_msg, data) {
+      if (data && data.length > 0) {
+        setNoseY(data[0].keypoints[0].y);
+      }
+    };
+    var token = PubSub.subscribe('poses', mySubscriber);
+    return () => PubSub.unsubscribe(token);
+  }, [])
+  return <>
+    <div>{JSON.stringify(noseY)}</div>
+  </>
+}
+
+
 const App = () => {
+  console.log("[top-level] rendered");
   const [clicked, setClicked] = React.useState(0);
   const saver = React.useRef(new Saver());
   console.log(saver.current);
+
+
   return <>
     <p>Clicked {clicked} times </p>
     <ExampleComponent handlePoses={(poses) => {
-      if (saver.current && poses) {
-        saver.current.frames.push(poses);
-      }
+      PubSub.publish('poses', poses);
     }} text="Create React Library Example 😄" />
     <button onClick={() => setClicked(clicked + 1)}>Click</button>
+    <SubComponent />
   </>
 }
 
